cb135e9dfa26d625cac2717933bff8edb9285fcf
Jiwoo Lee <110823848+leeejw00@users.noreply.github.com>
2024-02-27 00:26:58 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 132 ms, Memory: 15592 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,63 +1,61 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	public static int MAX = Integer.MIN_VALUE;	
-	public static int MIN = Integer.MAX_VALUE;	 
-	public static int[] operator = new int[4];	
-	public static int[] number;					 
-	public static int N;						
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		N = Integer.parseInt(br.readLine());
-		number = new int[N];
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			number[i] = Integer.parseInt(st.nextToken());
-		}
- 
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < 4; i++) {
-			operator[i] = Integer.parseInt(st.nextToken());
-		}
- 
-		dfs(number[0], 1);
- 
-		System.out.println(MAX);
-		System.out.println(MIN);
- 
-	}
- 
-	public static void dfs(int num, int idx) {
-		if (idx == N) {
-			MAX = Math.max(MAX, num);
-			MIN = Math.min(MIN, num);
-			return;
-		}
- 
-		for (int i = 0; i < 4; i++) {
-			if (operator[i] > 0) {
- 
-				operator[i]--;
- 
-				switch (i) {
- 
-				case 0:	dfs(num + number[idx], idx + 1);	break;
-				case 1:	dfs(num - number[idx], idx + 1);	break;
-				case 2:	dfs(num * number[idx], idx + 1);	break;
-				case 3:	dfs(num / number[idx], idx + 1);	break;
- 
-				}
-				operator[i]++;
-			}
-		}
-	}
- 
+    static int[] nums;
+    static int[] op; //연산자 배열 (+, - , *, /)
+    static int N;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        nums = new int[N];
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        op = new int[4];
+         for(int i=0; i<4; i++){
+             op[i] = Integer.parseInt(st.nextToken());
+         }
+
+         dfs(nums[0],1);
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    //연산자 개수만큼 완전탐색하여 모든 경우의 수 탐색
+    private static void dfs(int num, int idx) {
+        if(idx == N) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            return;
+        }
+
+        for(int i=0; i<4; i++){
+            if(op[i] > 0){
+                op[i]--; //해당 연산자 사용했으니 감소
+
+                if(i == 0)
+                    dfs(num + nums[idx], idx+1);
+                else if(i == 1)
+                    dfs(num - nums[idx], idx+1);
+                else if(i == 2)
+                    dfs(num * nums[idx], idx+1);
+                else
+                    dfs(num / nums[idx], idx+1);
+
+                op[i]++; //재귀 끝나고 나오면 연산자 개수 복구
+            }
+        }
+    }
 }
