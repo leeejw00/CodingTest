550aac01610be89839e375b17477ca33aa1b9ca3
Jiwoo Lee <110823848+leeejw00@users.noreply.github.com>
2024-07-06 17:29:14 +0900

    [Silver III] Title: N과 M (6), Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] nums;
+    static int[] result;
+    static boolean[] isSelected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        result = new int[M];
+        isSelected = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        comb(0, 0);
+        System.out.println(sb);
+    }
+
+    // 조합
+    private static void comb(int cnt, int idx) {
+        if(cnt == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = idx; i < N; i++) {
+            result[cnt] = nums[i];
+            comb(cnt+1, i+1);
+        }
+    }
+}
