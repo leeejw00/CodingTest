1cbaef91be2ed20bd5fabfe079a06e20f40936bb
Jiwoo Lee <110823848+leeejw00@users.noreply.github.com>
2024-08-06 14:50:30 +0900

    [level 1] Title: 모의고사, Time: 1.53 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,31 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] a = {1, 2, 3, 4, 5};
-        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        // 각 학생의 답안 패턴
+        int[][] patterns = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
 
-        int aCnt = 0, bCnt = 0, cCnt = 0;
+        // 학생 별 맞힌 문제 수를 저장할 배열
+        int cnt[] = new int[3];
+        
+        // 답안과 정답 비교
         int max = -1;
-        for(int i = 0; i < answers.length; i++) {
-            if(a[i % 5] == answers[i]) aCnt++;
-            if(b[i % 8] == answers[i]) bCnt++;
-            if(c[i % 10] == answers[i]) cCnt++;
+        for(int i = 0; i < cnt.length; i++) {
+            for(int j = 0; j < answers.length; j++) {
+                if(patterns[i][j % patterns[i].length] == answers[j])
+                    cnt[i]++;
+            }
         }
 
-        max = Math.max(aCnt, bCnt);
-        max = Math.max(max, cCnt);
+        // 최대 맞힌 문제 수
+        max = Math.max(Math.max(cnt[0], cnt[1]), cnt[2]);
 
         List<Integer> list = new ArrayList<>();
-        if (aCnt == max) list.add(1);
-        if (bCnt == max) list.add(2);
-        if (cCnt == max) list.add(3);
+        for(int i = 0; i < cnt.length; i++) {
+            if(max == cnt[i]) list.add(i + 1);
+        }
 
         int[] answer = new int[list.size()];
         for(int i = 0; i < answer.length; i++) {
             answer[i] = list.get(i);
         }
-        Arrays.sort(answer);
         return answer;
     }
 }
