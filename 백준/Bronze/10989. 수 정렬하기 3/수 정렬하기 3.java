f143e18f5160641e152cda10d4a68fe455404fe7
Jiwoo Lee <110823848+leeejw00@users.noreply.github.com>
2024-12-16 19:51:58 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1604 ms, Memory: 337436 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] count = new int[10001];
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            count[num]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= 10000; i++) {
+            while(count[i] > 0) {
+                sb.append(i).append("\n");
+                count[i]--;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
