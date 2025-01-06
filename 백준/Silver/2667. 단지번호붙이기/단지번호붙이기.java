54d773b3bbced1c0f1aceac736a9bd0b28626225
Jiwoo Lee <110823848+leeejw00@users.noreply.github.com>
2025-01-06 16:51:15 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,80 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int N;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[] dr = {-1, 1, 0, 0};//상하좌우
-	static int[] dc = {0, 0, -1, 1};//상하좌우
-	static int cnt; //단지 수
-	static int house; //단지 내 집의 수
-	static List<Integer> list; //단지 별 집의 수 저장
-	
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		map = new int[N][N];
-		visited = new boolean[N][N];
-		
-		for(int i=0; i<N; i++) {
-			String str = br.readLine();
-			for(int j=0; j<N; j++) {
-				map[i][j] = str.charAt(j) - '0';
-			}
-		}
-		
-		list = new ArrayList<Integer>();
-		
-		cnt = 0;
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				if(map[r][c]==1 && !visited[r][c]) {
-					visited[r][c] = true;
-					house = 1; //시작하는 집 1부터 세주기
-					bfs(r,c);
-					cnt++; //단지 수 증가
-					list.add(house);
-				}
-			}
-		}
-		Collections.sort(list);//오름차순 정렬
-		System.out.println(cnt);
-		for(int h : list) {
-			System.out.println(h);
-		}
-		
-	}
+    static int N, cnt;
+    static boolean[][] visited;
+    static int[][] map;
+    static int[] dr = {0, 0, -1, 1}; // 상 하 좌 우
+    static int[] dc = {-1, 1, 0, 0};
 
-	static void bfs(int row, int col) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {row,col});
-		
-		while(!q.isEmpty()) {
-			int[] curr = q.poll();
-			int r = curr[0];
-			int c = curr[1];
-			
-			for(int d=0; d<4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				
-				if(nr<0 || nc<0 || nc>=N || nr>=N) continue;
-				if(visited[nr][nc] || map[nr][nc]!=1) continue;
-				
-				visited[nr][nc] = true;
-				house++;
-				q.offer(new int[] {nr,nc});
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for(int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for(int j = 0; j < N; j++) {
+                map[i][j] = input.charAt(j) - '0';
+            }
+        }
+
+        List<Integer> houseList = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(map[i][j] == 1 && !visited[i][j]) {
+                    int houseCnt = bfs(i,j);
+                    houseList.add(houseCnt);
+                }
+            }
+        }
+        Collections.sort(houseList);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(houseList.size()).append("\n");
+
+        for(int houses : houseList) {
+            sb.append(houses).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static int bfs(int row, int col) {
+        visited[row][col] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {row, col});
+        cnt = 1;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            for(int d = 0; d < 4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+                if(nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+                if(map[nr][nc] == 0 || visited[nr][nc]) continue;
+
+                q.offer(new int[] {nr, nc});
+                visited[nr][nc] = true;
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
 }
