6054ee796db80639e460281a6d292fab51ba0acf
Jiwoo Lee <110823848+leeejw00@users.noreply.github.com>
2025-02-11 13:41:29 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int teamCnt = 0;
+        while (N >= 2 && M >= 1 && (N+M) - K >= 3) {
+            N -= 2;
+            M -= 1;
+
+            teamCnt++;
+        }
+
+        System.out.println(teamCnt);
+    }
+}
