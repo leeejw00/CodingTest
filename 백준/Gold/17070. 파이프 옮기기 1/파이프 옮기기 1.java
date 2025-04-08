e6aefd87fa2c9a8d1e280302a3f2aabc4e00c713
Jiwoo Lee <110823848+leeejw00@users.noreply.github.com>
2025-04-08 12:10:52 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 204 ms, Memory: 18836 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -7,24 +7,21 @@ public class Main {
     static int N;
     static int[][] map;
     static int ans = 0;
-    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
-
         map = new int[N+1][N+1];
+
+        StringTokenizer st;
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        visited = new boolean[N+1][N+1];
-
+        
         dfs(1, 2, 1); // 시작좌표 (1,2) (파이프 끝쪽 좌표)
 
         System.out.println(ans);
@@ -39,54 +36,28 @@ public class Main {
 
         // 현재 좌표에서 이동 가능한 경우 탐색
         if (pipe == 1) { // 가로인 경우
-            // 우 비어있으면 가로로 이동
-            if (c+1 <= N && !visited[r][c+1] && map[r][c+1] == 0) {
-                visited[r][c+1] = true;
+            if (c+1 <= N && map[r][c+1] == 0) {
                 dfs(r, c+1, 1);
-                visited[r][c+1] = false;
-            }
-
-            // 우, 우하, 하 비어있으면 대각선 이동
-            if (c+1 <= N && r+1 <= N && !visited[r+1][c+1] &&
-                   map[r][c+1] == 0 && map[r+1][c] == 0 && map[r+1][c+1] == 0) {
-                visited[r+1][c+1] = true;
-                dfs(r+1, c+1, 3);
-                visited[r+1][c+1] = false;
             }
         }
         else if (pipe == 2) { // 세로인 경우
-            if (r+1 <= N && !visited[r+1][c] && map[r+1][c] == 0) {
-                visited[r+1][c] = true;
+            if (r+1 <= N && map[r+1][c] == 0) {
                 dfs(r+1, c, 2);
-                visited[r+1][c] = false;
-            }
-
-            if (c+1 <= N && r+1 <= N && !visited[r+1][c+1] &&
-                    map[r][c+1] == 0 && map[r+1][c] == 0 && map[r+1][c+1] == 0) {
-                visited[r+1][c+1] = true;
-                dfs(r+1, c+1, 3);
-                visited[r+1][c+1] = false;
             }
         }
         else if (pipe == 3) { // 대각선인 경우
-            if (c+1 <= N && !visited[r][c+1] && map[r][c+1] == 0) {
-                visited[r][c+1] = true;
+            if (c+1 <= N && map[r][c+1] == 0) {
                 dfs(r, c+1, 1);
-                visited[r][c+1] = false;
             }
 
-            if (r+1 <= N && !visited[r+1][c] && map[r+1][c] == 0) {
-                visited[r+1][c] = true;
+            if (r+1 <= N && map[r+1][c] == 0) {
                 dfs(r+1, c, 2);
-                visited[r+1][c] = false;
             }
+        }
 
-            if (c+1 <= N && r+1 <= N && !visited[r+1][c+1] &&
-                    map[r][c+1] == 0 && map[r+1][c] == 0 && map[r+1][c+1] == 0) {
-                visited[r+1][c+1] = true;
-                dfs(r+1, c+1, 3);
-                visited[r+1][c+1] = false;
-            }
+        // 모든 경우에서 대각선 방향 탐색
+        if (c+1 <= N && r+1 <= N && map[r][c+1] == 0 && map[r+1][c] == 0 && map[r+1][c+1] == 0) {
+            dfs(r+1, c+1, 3);
         }
     }
 }
