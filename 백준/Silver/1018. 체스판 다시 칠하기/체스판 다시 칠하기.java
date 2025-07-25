e41a5e94d267532d1b9bbdec653d8e168ae9296b
Jiwoo Lee <110823848+leeejw00@users.noreply.github.com>
2025-07-25 16:12:05 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,54 +4,61 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static boolean[][] map;
-	static int min = 64; //8*8
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		map = new boolean[n][m];
-		for(int i=0; i<n; i++) {
-			String colors = br.readLine();
-			for(int j=0; j<m; j++) {
-				if(colors.charAt(j) == 'W') {
-					map[i][j] = true; //w는 true, b는 false로 저장
-				}else {
-					map[i][j] = false;
-				}
-			}
-		}
-		
-		//확인 가능 시작 범위
-		for(int i=0; i<n-7; i++) {
-			for(int j=0; j<m-7; j++) {
-				find(i,j);
-			}
-		}
-		
-		System.out.println(min);
-	
-	}
+    static int min = 64;
+    static boolean[][] map;
 
-	static void find(int x, int y) {
-		int cnt = 0;
-		boolean check = map[x][y]; //체스판 첫번째 칸의 색 저장 (T/F)
-		
-		for(int i=x; i<x+8; i++) {
-			for(int j=y; j<y+8; j++) {
-				if(map[i][j] != check) {
-					cnt++;
-				}
-				check = !check; //한 칸씩 색 변경되어야 함
-			}
-			check = !check;
-		}
-		
-		cnt = Math.min(cnt, 64-cnt); //현재와 반대의 색깔 기준으로 바꿔 칠했을 경우 비교하여 최소값 저장
-		min = Math.min(min, cnt);	
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        map = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+
+            for (int j = 0; j < M; j++) {
+                if (input.charAt(j) == 'W') {
+                    map[i][j] = true; // W 이면 true
+                } else {
+                    map[i][j] = false; // B 이면 false
+                }
+            }
+        }
+
+        // 8*8 로 잘라서 최소값 구하기
+        for (int i = 0; i <= N-8; i++) {
+            for (int j = 0; j <= M-8; j++) {
+                find(i,j);
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    static void find(int x, int y) {
+        int cnt = 0;
+
+        boolean TF = map[x][y]; // 첫 번째 칸의 색
+
+        for (int i = x; i < x + 8; i++) {
+            for (int j = y; j < y + 8; j++) {
+
+                // 올바른 색이 아니면 cnt 증가
+                if (map[i][j] != TF) {
+                    cnt++;
+                }
+
+                // 다음 칸은 무조건 색이 반대로 바뀐다
+                TF = !TF;
+            }
+            // 한 행이 바뀔 때는 전 행의 마지막 색과 그대로여야 함
+            TF = !TF;
+        }
+
+        // 첫번째 칸이 반대 색인 경우도 구해야 함
+        // 해당 경우의 색칠 할 개수는 64 - cnt
+        min = Math.min(min, Math.min(cnt, 64 - cnt));
+    }
 }
